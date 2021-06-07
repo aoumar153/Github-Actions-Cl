d5cc00d28a02613d91bea44360ad78a720640c78
Abdullah Oumar <oumarab@oregonstate.edu>
2021-06-06 18:54:10 -0700

    added test and multiplication code

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ class TestCase(unittest.TestCase):
 
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1,1),0)
+    
+    def test_multiply(self):
+        self.assertEqual(example.multiply(2,3),6)
 
 if __name__ == '__main__':
     unittest.main()
